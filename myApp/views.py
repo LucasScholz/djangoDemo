f7d29901569a6cf228d51aa990f668a6225c725d
Lucas Scholz <lucas.scholz2103@gmail.com>
2025-07-24 10:04:28 +0200

    - TodoItem Model hinzugefügt - Url für todo item hinzugefügt bei urls.py - view mit den item Objekten hinzugefügt

--- myApp/views.py
+++ myApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from .models import TodoItem
+
 
 # Create your views here.
 
@@ -7,3 +9,6 @@ def home(request):
     return render(request, "home.html")
 
 
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, "todos.html", {"todos": items})
