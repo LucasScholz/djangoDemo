b5d4bfe1bdd96ef386650a8d82bf769780c0c25a
Lucas Scholz <lucas.scholz2103@gmail.com>
2025-07-24 11:09:01 +0200

    Todo hinzufügen Page wurde hinzugefüt und funktioniert

--- myApp/views.py
+++ myApp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import TodoItem
+from .forms import TodoItemForm
 
 
 # Create your views here.
@@ -12,3 +13,13 @@ def home(request):
 def todos(request):
     items = TodoItem.objects.all()
     return render(request, "todos.html", {"todos": items})
+
+def add_todo(request):
+    if (request.method == 'POST'):
+        form = TodoItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todos')
+    else:
+        form = TodoItemForm()
+    return render(request, 'add_todo.html', {'form': form})
